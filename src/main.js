54c73a08ded920b4c3dbcb18fa55aa67de82358d
mini-inim <forestaroundseoul@gmail.com>
2025-03-23 23:28:50 +0900

    라우터 생성

--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
+const state = {
+  isLogin: false,
+};
+
+const Header = ({ isLogin }) => `
+  <header class="bg-blue-600 text-white p-4 sticky top-0">
+    <h1 class="text-2xl font-bold">항해플러스</h1>
+  </header>
+
+  <nav class="bg-white shadow-md p-2 sticky top-14">
+  <ul class="flex justify-around">
+    <li><a href="/" class="text-blue-600">홈</a></li>
+    ${
+      isLogin
+        ? `
+      <li><a href="/profile" class="text-gray-600">프로필</a></li>
+      <li><a href="#" class="text-gray-600">로그아웃</a></li>`
+        : `
+      <li><a href="/login" class="text-gray-600">로그인</a></li>
+    `
+    }
+    </ul>
+  </nav>
+
+`;
+
+const Footer = () => `
+  <footer class="bg-gray-200 p-4 text-center">
+  <p>&copy; 2024 항해플러스. All rights reserved.</p>
+  </footer>
+`;
+
 const MainPage = () => `
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
-      <header class="bg-blue-600 text-white p-4 sticky top-0">
-        <h1 class="text-2xl font-bold">항해플러스</h1>
-      </header>
-
-      <nav class="bg-white shadow-md p-2 sticky top-14">
-        <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
-          <li><a href="/profile" class="text-gray-600">프로필</a></li>
-          <li><a href="#" class="text-gray-600">로그아웃</a></li>
-        </ul>
-      </nav>
-
+    ${Header({ isLogin: state.isLogin })}
       <main class="p-4">
         <div class="mb-4 bg-white rounded-lg shadow p-4">
           <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
@@ -102,10 +123,7 @@ const MainPage = () => `
           </div>
         </div>
       </main>
-
-      <footer class="bg-gray-200 p-4 text-center">
-        <p>&copy; 2024 항해플러스. All rights reserved.</p>
-      </footer>
+      ${Footer()}
     </div>
   </div>
 `;
@@ -233,9 +251,20 @@ const ProfilePage = () => `
   </div>
 `;
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+const render = () => {
+  document.body.innerHTML = App();
+};
+
+const App = () => {
+  if (location.pathname === "/login") {
+    return LoginPage();
+  } else if (location.pathname === "/profile") {
+    return ProfilePage();
+  } else if (location.pathname === "/") {
+    return MainPage();
+  } else {
+    return ErrorPage();
+  }
+};
+
+render();
