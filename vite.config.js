8b76392e64557e1690045ecf31285648ba8c7e2c
mini-inim <forestaroundseoul@gmail.com>
2025-03-28 07:52:22 +0900

    배포 과정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from "vitest/config";
 import { resolve } from "path";
 
-export default defineConfig(({ mode }) => {
-  return {
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.js",
-      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-          hash: resolve(__dirname, "index.hash.html"),
-        },
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        hash: resolve(__dirname, "index.hash.html"),
       },
     },
-    base: mode === "production" ? "/front_5th_chapter1-1/" : "/",
-  };
+  },
+  base: isProduction ? "/front_5th_chapter1-1/" : "/",
 });
