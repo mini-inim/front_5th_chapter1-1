028f1b9a378bf069a86c2e17ccc42064f64d4d28
mini-inim <forestaroundseoul@gmail.com>
2025-03-28 07:42:37 +0900

    base 수정

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from "vitest/config";
 import { resolve } from "path";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        hash: resolve(__dirname, "index.hash.html"),
+export default defineConfig(({ mode }) => {
+  return {
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          hash: resolve(__dirname, "index.hash.html"),
+        },
       },
     },
-  },
-  base: isProduction ? "/front_5th_chapter1-1" : "/",
+    base: mode === "production" ? "/front_5th_chapter1-1/" : "/",
+  };
 });
